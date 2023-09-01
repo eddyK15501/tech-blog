94ff9a98bfe31fcfda93395bb904700c5a838775
Edward Kim <itgma1231@gmail.com>
2023-09-01 12:25:06 -0400

    sequelize.js connected; .env variables filled

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-const express = require('express')
+const express = require("express");
+
+const sequelize = require("./config/connection");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, () => {
-    console.log("Server now listening.")
-})
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log("Server now listening.");
+  });
+});
