1ae22600e72feb06b402aa47feaf5a38205ff775
eddyK15501 <edk1231@gmail.com>
2023-09-01 20:25:30 -0400

    handlebars.js sinitial setup; simple navbar created;

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 const express = require("express");
+const path = require('path')
+
+const exphbs = require('express-handlebars')
 
 const sequelize = require("./config/connection");
 
+const routes = require('./routes')
+
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const hbs = exphbs.create();
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars')
+
+app.use(express.static(path.resolve(__dirname, 'public')))
+
+app.use(routes)
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => {
