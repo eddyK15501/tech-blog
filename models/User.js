9874ff033933032a808af0cfef861427439a2476
Edward Kim <itgma1231@gmail.com>
2023-09-02 00:10:15 -0400

    User.js model created

---
+++ models/User.js
@@ -0,0 +1,53 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+const bcrypt = require("bcrypt");
+
+class User extends Model {
+  async validatePassword(login) {
+    try {
+      return await bcrypt.compare(login, this.password);
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
+}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [5, 30],
+      },
+    },
+  },
+  {
+    hooks: {
+      async beforeCreate(newUser) {
+        newUser.password = await bcrypt.hash(newUser.password, 10);
+        return newUser;
+      },
+      async beforeUpdate(updatedUser) {
+        updatedUser.password = await bcrypt.hash(updatedUser.password, 10);
+        return updatedUser;
+      },
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "user",
+  }
+);
