431365163ac51aa17efa8dd8bb12045dfe8f2a36
eddyK15501 <edk1231@gmail.com>
2023-09-03 14:01:51 -0400

    npm install express-session connect-session-sequelize; server.js setup

--- server.js
+++ server.js
@@ -1,26 +1,38 @@
-const express = require("express");
-const path = require('path')
+const express = require('express');
+const path = require('path');
+const session = require('express-session');
+const handlbars = require('express-handlebars');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-const exphbs = require('express-handlebars')
-
-const sequelize = require("./config/connection");
-
-const routes = require('./routes')
+const routes = require('./routes');
+const sequelize = require('./configs/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const hbs = exphbs.create();
+const sess = {
+  secret: 'secret session key',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess))
+
+const hbs = handlbars.create();
 
 app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars')
+app.set('view engine', 'handlebars');
 
-app.use(express.static(path.resolve(__dirname, 'public')))
+app.use(express.static(path.resolve(__dirname, 'public')));
 
-app.use(routes)
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => {
-    console.log("Server now listening.");
+    console.log('Server now listening.');
   });
 });
