645c6c3a572557bd39da0a9680f3fab6b8edcf81
Edward Kim <itgma1231@gmail.com>
2023-09-07 10:03:57 -0400

    dashboard-routes on '/'; ready to render

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -3,16 +3,20 @@ const { User, Post, Comment } = require("../models");
 const withAuth = require("../utils/auth");
 
 router.get("/", async (req, res) => {
-  const dbPostData = await Post.findAll({
-    where: { user_id: req.session.user_id },
-  });
+  try {
+    const dbPostData = await Post.findAll({
+      where: { user_id: req.session.user_id },
+    });
 
-  const data = dbPostData.map((post) => {
-    return post.get({ plain: true });
-  });
-  console.log(data);
+    const data = dbPostData.map((post) => {
+      return post.get({ plain: true });
+    });
 
-  res.render("dashboard", { data, loggedIn: req.session.loggedIn });
+    res.render("dashboard", { data, loggedIn: req.session.loggedIn });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
