21dab9638eddf7349216c24cede03d08dae8f3ff
Edward Kim <itgma1231@gmail.com>
2023-09-08 15:33:07 -0400

    npm run seeds for heroku deployment

--- seeds/commentData.js
+++ seeds/commentData.js
@@ -1,9 +1,9 @@
 const { Comment } = require("../models");
 
 const newComment = {
-  comment_text: "You'll Do N00TiN, YaH LiTL weeZeL.",
-  user_id: "5",
-  post_id: "2",
+  comment_text: "Wh0daF00k is diS Guy? You'll Do N00TiN, yaH LiTL weeZeL.",
+  user_id: 5,
+  post_id: 3,
 };
 
 const seedComments = () => Comment.create(newComment);
--- seeds/postData.js
+++ seeds/postData.js
@@ -4,23 +4,28 @@ const newPosts = [
   {
     title: 'Handlebars.js',
     content: 'Templating engines are quite outdated now days.',
-    user_id: '2',
+    user_id: 2,
+  },
+  {
+    title: 'Fight Me.',
+    content: "yaLiTLweezel.",
+    user_id: 5,
   },
   {
     title: 'Speaking of templating engines...',
     content: 'curse be to this damn thing...dare challenge my indestructible wisdom.',
-    user_id: '4',
+    user_id: 4,
   },
   {
     title: 'When designing an MVC pattern...',
     content: 'Start with the model, followed by the controller, then the view. Ya dig?',
-    user_id: '3',
+    user_id: 3,
   },
   {
     title: 'Coding bootcamps',
     content:
       "You'll be drowned in web development, in such a short amount of time. As difficult as it is, I can definitely say that I've learned a lot.",
-    user_id: '1',
+    user_id: 1,
   },
 ];
 
